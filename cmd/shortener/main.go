50440e6ff139da0464ed44ca7074178503b5c6f4
Владимир козин <kozn@aicrobotics.ru>
2022-10-06 23:11:19 +0300

    Создание кода ссылки и роутер

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,49 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"math/rand"
+	"net/http"
+)
+
+const (
+	alphabet   = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	length     = len(alphabet)
+	CodeLength = 6
+)
+
+// Обработчик.
+func run(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+
+		encode := NewCode()
+
+		w.Write([]byte(encode))
+	}
+
+	if r.Method == http.MethodPost {
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte("Привет  "))
+	}
+
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", run)
+
+	log.Println("Запуск веб-сервера на http://127.0.0.1:8080")
+	err := http.ListenAndServe(":8080", mux)
+	log.Fatal(err)
+}
+
+func NewCode() string {
+	var letters = []rune(alphabet)
+
+	code := make([]rune, CodeLength)
+	for i := range code {
+		code[i] = letters[rand.Intn(length)]
+	}
+
+	return string(code)
+}
