7f0143923ce938533d35c69bb77e159776038264
Владимир козин <kozn@aicrobotics.ru>
2022-10-14 21:40:57 +0300

    Забыл добавить префикс api в роут

--- internal/app/handler/handler_test.go
+++ internal/app/handler/handler_test.go
@@ -138,7 +138,7 @@ func TestShortHandler(t *testing.T) {
 				contentType: "",
 				method:      http.MethodPost,
 				body:        "",
-				path:        "/shorten",
+				path:        "/api/shorten",
 			},
 		},
 		{
@@ -148,7 +148,7 @@ func TestShortHandler(t *testing.T) {
 				contentType: "",
 				method:      http.MethodPost,
 				body:        "{  \"url\" : \"https://yandex.ru/search/?text=golang+%D0%B4%D0%BE%D1%81%D1%82%D1%83%D1%82%D1%8C+%D0%B8%D0%B7+%D1%82%D0%B5%D0%BB%D0%B0+%D0%B7%D0%B0%D0%BF%D1%80%D0%BE%D1%81%D0%B0&lr=35\"}",
-				path:        "/shorten",
+				path:        "/api/shorten",
 			},
 		},
 		{
@@ -158,7 +158,7 @@ func TestShortHandler(t *testing.T) {
 				contentType: "",
 				method:      http.MethodPost,
 				body:        "{  \"url\" : \"https://\"}",
-				path:        "/shorten",
+				path:        "/api/shorten",
 			},
 		},
 	}
