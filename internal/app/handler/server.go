68f89f493ecfe6809e97865647f40868cdd86e79
Владимир козин <kozn@aicrobotics.ru>
2022-10-23 12:49:18 +0300

    Установил content type по типу передаваемых данных

--- internal/app/handler/server.go
+++ internal/app/handler/server.go
@@ -26,6 +26,7 @@ func (s *Server) MountHandlers() {
 	s.Router.Use(middleware.Logger)
 	s.Router.Use(middleware.Recoverer)
 	s.Router.Use(middleware.AllowContentEncoding("gzip"))
+	s.Router.Use(middleware.AllowContentType("application/json", "text/plain", "application/x-gzip"))
 	s.Router.Use(middleware.Compress(5, "application/json", "text/plain", "text/html"))
 
 	s.Router.Post("/api/shorten", s.ShortHandler)
