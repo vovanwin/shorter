237937a9eb3e72a55f53e09cb8768263df28a4de
Владимир козин <kozn@aicrobotics.ru>
2022-10-23 12:34:21 +0300

    Удалил мидлваре с обязательный application/json

--- internal/app/handler/server.go
+++ internal/app/handler/server.go
@@ -26,7 +26,6 @@ func (s *Server) MountHandlers() {
 	s.Router.Use(middleware.Logger)
 	s.Router.Use(middleware.Recoverer)
 	s.Router.Use(middleware.AllowContentEncoding("gzip"))
-	s.Router.Use(middleware.AllowContentType("application/json"))
 	s.Router.Use(middleware.Compress(5, "application/json"))
 
 	s.Router.Post("/api/shorten", s.ShortHandler)
